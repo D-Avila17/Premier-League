9f0eb8a61a24241407cd08e20266847bfe55cde2
D-Avila17 <diego.avila4117@gmail.com>
2023-04-27 09:06:06 -0700

    first edit

--- premmain.py
+++ premmain.py
@@ -4,3 +4,19 @@
 create a program that helps me predict who will win the Premier League
 and get relegated based on existing data
 '''
+# webbrowser opens a browser to a specific page
+import webbrowser
+webbrowser.open('https://automatetheboringstuff.com/2e/chapter12/')
+
+#! python 3
+# premmain.py - launches a table in the browser using data from the command line or clipboard
+
+import webbrowser, sys, pyperclip
+if len(sys.argv) > 1:
+    # Get address from command line
+    address = ''.join(sys.argv[1:])
+else:
+    # Get address from clipboard
+    address = pyperclip.paste()
+
+
